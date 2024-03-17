cce4d4fb4ac326e46810c406fde09343105c573f
smtdeveloper <sametakcatoki@gmail.com>
2024-03-17 20:46:50 +0300

    ServiceGeneric

---
+++ PolyPurrApp.Services/DtoMapper.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using PolyPurrApp.Core.Dtos;
+using PolyPurrApp.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolyPurrApp.Services
+{
+    internal class DtoMapper : Profile
+    {
+        public DtoMapper()
+        {
+            CreateMap<UserAppDto, User>().ReverseMap();
+        }
+    }
+}
